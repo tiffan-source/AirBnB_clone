4d3c99310987e2c0ce8491c6ddde9f59388e4a54
Tiffane <zohoungbogboanlyou@gmail.com>
2022-10-31 12:47:57 +0100

    add more test on other class

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,6 +4,7 @@ test_review module : Test behavior of Review Class
 """
 import unittest
 from models.city import City
+from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
@@ -17,3 +18,14 @@ class TestCity(unittest.TestCase):
     def test_module_base_documentation(self):
         import models.city
         self.assertIsNotNone(models.city.__doc__)
+
+    def test_city_deriv_base(self):
+        self.assertTrue(issubclass(City, BaseModel))
+
+    def test_city_field(self):
+        self.assertIsInstance(City.state_id, str)
+        self.assertIsInstance(City.name, str)
+
+    def test_city_field_value(self):
+        self.assertEqual("", City.name)
+        self.assertEqual("", City.state_id)
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -4,6 +4,7 @@ test_review module : Test behavior of Review Class
 """
 import unittest
 from models.place import Place
+from models.base_model import BaseModel
 
 
 class TestPlace(unittest.TestCase):
@@ -17,3 +18,32 @@ class TestPlace(unittest.TestCase):
     def test_module_base_documentation(self):
         import models.place
         self.assertIsNotNone(models.place.__doc__)
+
+    def test_place_deriv_base(self):
+        self.assertTrue(issubclass(Place, BaseModel))
+
+    def test_place_field(self):
+        self.assertIsInstance(Place.city_id, str)
+        self.assertIsInstance(Place.user_id, str)
+        self.assertIsInstance(Place.name, str)
+        self.assertIsInstance(Place.description, str)
+        self.assertIsInstance(Place.number_rooms, int)
+        self.assertIsInstance(Place.number_bathrooms, int)
+        self.assertIsInstance(Place.max_guest, int)
+        self.assertIsInstance(Place.price_by_night, int)
+        self.assertIsInstance(Place.latitude, float)
+        self.assertIsInstance(Place.longitude, float)
+        self.assertIsInstance(Place.amenity_ids, list)
+
+    def test_place_field_value(self):
+        self.assertEqual("", Place.city_id)
+        self.assertEqual("", Place.user_id)
+        self.assertEqual("", Place.name)
+        self.assertEqual("", Place.description)
+        self.assertEqual(0, Place.number_rooms)
+        self.assertEqual(0, Place.number_bathrooms)
+        self.assertEqual(0, Place.max_guest)
+        self.assertEqual(0, Place.price_by_night)
+        self.assertEqual(0.0, Place.latitude)
+        self.assertEqual(0.0, Place.longitude)
+        self.assertEqual([], Place.amenity_ids)
